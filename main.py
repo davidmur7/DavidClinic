fd161dd394278067557fb5cd121c59402dba35de
davidmur7 <davidmurphy1241@gmail.com>
2024-04-07 14:12:20 +0100

    datetime, input control

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
 # Author: David Murphy
+from datetime import datetime
+current_datetime = datetime.now()
+current_date = current_datetime.date()
+print(current_date)
 def main():
     schedule_appointment()
 def schedule_appointment():
     name = input("Enter your name: ")
-    contact_number = int(input("Enter your phone number: "))
+    while len(name) > 20 or len(name) < 1:
+        name = input("Enter your name: ")
+    contact_number = input("Enter your phone number: ")
+    while len(contact_number) != 10 or not contact_number.isdigit():
+        print("Contact number needs to be a number of exactly 10 digits ")
+        contact_number = int(input("Enter your phone number: "))
     appointment_date = input("Enter appointment date (d/mm/yyyy) : ")
     appointment_time = input("Enter your prefered time of appointment (hour:minute) : ")
+
+main()
