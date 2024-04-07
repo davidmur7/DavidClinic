2e4bcba0287c98082eeccda15c4937c241eedc6b
davidmur7 <davidmurphy1241@gmail.com>
2024-04-07 13:53:23 +0100

    first 2 functions

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
-print("hshshs")
-print("dhdhdhhd")
-print("dhdhhd")
+# Author: David Murphy
+def main():
+    schedule_appointment()
+def schedule_appointment():
+    name = input("Enter your name: ")
+    contact_number = int(input("Enter your phone number: "))
+    appointment_date = input("Enter appointment date (d/mm/yyyy) : ")
+    appointment_time = input("Enter your prefered time of appointment (hour:minute) : ")
